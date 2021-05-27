b09cd817d7b2c0f51be96a345c34344b4f5f1106
RalfEs73 <ralf@essbaumer.de>
2021-05-27 16:01:16 +0200

    New Version 21.3.0

--- movavivideoconverter/tools/chocolateyinstall.ps1
+++ movavivideoconverter/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://files.movavi.com/x32/MovaviVideoConverterSetupE.exe'
-$url64      = 'https://files.movavi.com/x64/MovaviVideoConverterSetupE.exe'
+$url64      = 'https://cdn.movavi.com/x64/MovaviVideoConverterSetupE.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'movavivideoconverter*'
-  checksum      = '01DDD9DD9BBA37ABE17042071B7E41136A11348F46A054A3A25FBBCC2F86EAAA'
+  checksum      = 'CCDDC9CF0D42F1D9782249F5093B29D59B9919C0D77370DE813E3CB3D50A1A60'
   checksumType  = 'sha256'
-  checksum64    = '01DDD9DD9BBA37ABE17042071B7E41136A11348F46A054A3A25FBBCC2F86EAAA'
+  checksum64    = 'F501DCD206101F0E417BD7990C95C479D91511AE3B4D33726AB6231673717F43'
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
