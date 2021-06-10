87566609da75bc22d135e4f07376698e80b18b65
RalfEs73 <ralf@essbaumer.de>
2021-06-10 09:44:19 +0200

    New Version 1.5

--- hasleobackupsuite/tools/chocolateyinstall.ps1
+++ hasleobackupsuite/tools/chocolateyinstall.ps1
@@ -10,10 +10,10 @@ $packageArgs = @{
 
   softwareName  = 'hasleobackupsuite*'
 
-  checksum      = '9B159EC844BA1911BE9B028E6C523289BB840B8AD371B865A0398366F0961A31'
+  checksum      = 'E18B0ED9C173AD2E9D57700EFF267BAE275B5D642917AC00F78AB95B73724BFD'
   checksumType  = 'sha256'
 
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyPackage @packageArgs
