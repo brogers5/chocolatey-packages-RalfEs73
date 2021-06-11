3e87b5bbd1ac1c0d28818da47c22f6d902a801ad
RalfEs73 <ralf@essbaumer.de>
2021-06-11 09:14:47 +0200

    New Version 2.29.0

--- ledger-live/tools/chocolateyinstall.ps1
+++ ledger-live/tools/chocolateyinstall.ps1
@@ -1,12 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v2.26.1/ledger-live-desktop-2.26.1-win.exe'
+$url64      = 'https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v2.29.0/ledger-live-desktop-2.29.0-win.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64bit      = $url64
   softwareName  = 'ledger-live*'
   checksum64    = '1953B584161C99CD5627F80A345E6C8A83A201AA56A05912C1DE673E22B83795'
