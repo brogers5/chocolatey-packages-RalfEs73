5533fe1c875a89dfa4e12cc90245c50a3919bd32
RalfEs73 <ralf@essbaumer.de>
2021-08-21 16:47:05 +0200

    New Version 2.6.2

---
+++ defichainwallet/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/DeFiCh/app/releases/download/v2.6.2/DeFi-Wallet-Setup-2.6.2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64bit      = $url64
+  softwareName  = 'defichainwallet*'
+  checksum64    = '06026CE671328075D34A00A8CF28000672C7735A78CC3844D3C42D3CA220E68A'
+  checksumType64= 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
