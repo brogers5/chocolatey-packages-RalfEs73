f2ebf034dd3ddbff95daf8afa342ba92eb3dcbbe
RalfEs73 <ralf@essbaumer.de>
2021-10-13 12:53:18 +0200

    New Version 1.18.3

---
+++ volksverschluesselung/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl1.volksverschluesselung.de/VV-Private-Client-Windows-Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'volksverschluesselung*'
+
+  checksum      = '8B4CBCC1C32CC23360B598E267FED21BE393C9A933130364BC9A364AF69F8B38'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
