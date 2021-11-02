d8d7e57a318fe44c8be23dbb970709527e215b0d
RalfEs73 <ralf@essbaumer.de>
2021-11-02 13:35:19 +0100

    Add files via upload

---
+++ nditools/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://downloads.ndi.tv/Tools/NDI%205%20Tools.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'nditools*'
+
+  checksum      = 'A39DC09052F519B28E9DA2F094373BF7B030E371846879FB20621A182D785C0B'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
