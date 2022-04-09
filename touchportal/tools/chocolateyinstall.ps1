251c98f712389a8026036f3d3623f4b96e4eb75c
RalfEs73 <ralf@essbaumer.de>
2022-04-09 19:24:34 +0200

    New Version 3.0.10

--- touchportal/tools/chocolateyinstall.ps1
+++ touchportal/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.touch-portal.com/downloads/TouchPortal_Setup.exe'
+$url        = 'https://www.touch-portal.com/downloads/TouchPortal_Setup_release.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'touchportal*'
 
-  checksum      = 'BA07DCC69DD39C98F95E24B60C2B6F39E91E3B9C5224C32FDAE35BE2FE5CE021'
+  checksum      = '8B4D58F3E5561964CED4455F33E56823A7B321FF048B03C9A775AD3F3AF34A5F'
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
