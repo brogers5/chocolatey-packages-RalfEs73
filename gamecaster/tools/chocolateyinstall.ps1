aba11575c7c3f22fda967c5b029279b59e212bf6
RalfEs73 <ralf@essbaumer.de>
2022-04-19 19:14:33 +0200

    New Version 4.0.2109.2802

--- gamecaster/tools/chocolateyinstall.ps1
+++ gamecaster/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.gamecaster.com/4.0.2012.0902/Gamecaster_Installer_4.0.2012.0902.exe?'
+$url        = 'https://downloads.gamecaster.com/4.0.2109.2802/Gamecaster_Installer_4.0.2109.2802.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   url           = $url
 
   softwareName  = 'gamecaster*'
-  checksum      = 'DE4183376F12FAF5001D68AC551A393E1F4AB7AF6BD85CD854FE6CA88F5BF9EB'
+  checksum      = '92BA9954B87E7EE9F50E422D4FF3013C23CE68B7113C6B5095586EB6B2786ABF'
   checksumType  = 'sha256'
   silentArgs    = "/exenoupdates /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
