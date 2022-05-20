db30bf6af909863d3a0fc1491c7c3fce73a46d23
RalfEs73 <ralf@essbaumer.de>
2022-05-20 17:29:32 +0200

    New Version 7.5.2.5

--- cloudberrybackup/tools/chocolateyinstall.ps1
+++ cloudberrybackup/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.msp360.com/CloudBerryBackup_v7.5.1.7DESKTOP.exe'
+$url        = 'https://download.msp360.com/CloudBerryBackup_v7.5.2.5DESKTOP.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   
   softwareName  = 'cloudberrybackup*'
 
-  checksum      = '6D06B261AB035B6CE2A92CEE755F24CD586BB1978AD2DAC932D3FFBE4F027AEC'
+  checksum      = 'A365383D5348FEEC093099D061F8EE27F63B15A1E42E24DFF7307C165F9F0B87'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
