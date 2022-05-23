75fd779b2d8912ef7d556e0cf92cba479cac618a
RalfEs73 <ralf@essbaumer.de>
2022-05-23 11:04:29 +0200

    New Version 1.3.6.0

--- anystream/tools/chocolateyinstall.ps1
+++ anystream/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.redfox.bz/download/SetupAnyStream_1.3.5.2_32bit.exe'
-$url64      = 'https://www.redfox.bz/download/SetupAnyStream_1.3.5.2_64bit.exe'
+$url        = 'https://www.redfox.bz/download/SetupAnyStream_1.3.6.0_32bit.exe'
+$url64      = 'https://www.redfox.bz/download/SetupAnyStream_1.3.6.0_64bit.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'anystream*'
-  checksum      = 'BED18CDFA6799A14FF33EB13F2ADC50DAE430E6BDA395050ACB4F3A2F7010880'
+  checksum      = '7D5A19EB229B3F4C9126B7900DEF25BA4F020E0DA566CC276772309AFFB0FBBF'
   checksumType  = 'sha256'
-  checksum64    = '5540A42B0E36033481A7DADBCA7BD4A90278F53E8C730FA1D10D2A5C4D96F668'
+  checksum64    = '09293B312CA80DAD2EC08FE9E0E5C35497EDBAAF040F8FEDFB652A3CB83B9BEF'
   checksumType64= 'sha256'
   silentArgs   = '/S'
 }
