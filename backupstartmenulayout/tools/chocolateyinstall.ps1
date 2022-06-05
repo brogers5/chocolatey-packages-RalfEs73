ca670f8c34bba17625cc19f70a670809a137af71
RalfEs73 <ralf@essbaumer.de>
2022-06-05 18:08:15 +0200

    Just testing

---
+++ backupstartmenulayout/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'backupstartmenulayout'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Shortcut    = 'Backup Start Menu Layout.lnk'
+$ProgramEXE  = 'BackupSML_x64.exe'
+
+$packageArgs = @{
+    PackageName    = $packageName
+    FileFullPath   = "$toolsPath\BackupSML.zip"
+    Destination    = $toolsDir
+}
+
+Get-ChocolateyWebFile -PackageName 'planetmule' -FileFullPath "$toolsPath\BackupSML.zip" -Url 'https://www.sordum.org/files/download/backup-start-menu-layout/BackupSML.zip'
+Get-ChocolateyUnzip @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\Mule\$ProgramEXE"    
---
+++ backupstartmenulayout/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'planetmule' 
+$Shortcut    = 'Mule.lnk'
+
+Remove-Item -Path "$env:Public\Desktop\$Shortcut"
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut"
