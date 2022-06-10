740f8042511c1e5b9479088520efdbdd738ac6f6
RalfEs73 <ralf@essbaumer.de>
2022-06-10 15:38:03 +0200

    New Version 1.5

--- backupstartmenulayout/tools/chocolateyinstall.ps1
+++ backupstartmenulayout/tools/chocolateyinstall.ps1
@@ -5,6 +5,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $Shortcut    = 'Backup Start Menu Layout.lnk'
 $ProgramEXE  = 'BackupSML_x64.exe'
+$Checksum    = '6455092ABCABBA7E46BC933C1909ADBFAEDD57096D0ED0D6ED38439DAF396C81'
 
 $packageArgs = @{
     PackageName    = $packageName
@@ -12,7 +13,7 @@ $packageArgs = @{
     Destination    = $toolsDir
 }
 
-Get-ChocolateyWebFile -PackageName 'backupstartmenulayout' -FileFullPath "$toolsPath\BackupSML.zip" -Url 'https://www.sordum.org/files/download/backup-start-menu-layout/BackupSML.zip'
+Get-ChocolateyWebFile -PackageName 'backupstartmenulayout' -Checksum $Checksum -FileFullPath "$toolsPath\BackupSML.zip" -Url 'https://www.sordum.org/files/download/backup-start-menu-layout/BackupSML.zip'
 Get-ChocolateyUnzip @packageArgs
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\BackupSML\$ProgramEXE"    
