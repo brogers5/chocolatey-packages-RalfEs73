8c5f69d4dc03a91b0a3981d931b6d333cb60da58
RalfEs73 <ralf@essbaumer.de>
2022-07-02 20:57:16 +0200

    New Version 2.15.0

--- defiwallet/tools/chocolateyinstall.ps1
+++ defiwallet/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/DeFiCh/app/releases/download/v2.14.0/DeFi-Wallet-Setup-2.14.0.exe'
+$url64      = 'https://github.com/DeFiCh/app/releases/download/v2.15.0/DeFi-Wallet-Setup-2.15.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'defiwallet*'
-  checksum64    = '71EBD0489710F1405007DE13E5FF10C63FBCEDD7BDCF920E984AC2FE5D5FA0E7'
+  checksum64    = 'ACD9BA964F5FD097AAC7826D89E3F1908B14DE433D1A6EA7EC0866DDF4CAF1DA'
   checksumType64= 'sha256'
   silentArgs   = '/S'
 }
