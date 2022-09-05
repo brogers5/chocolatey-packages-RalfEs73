d900e9504250557dbcc0f8474f24b05ab4abd8e0
RalfEs73 <ralf@essbaumer.de>
2022-09-05 17:20:35 +0200

    First Version

--- copy-dialog-lunar-lander/tools/chocolateyinstall.ps1
+++ copy-dialog-lunar-lander/tools/chocolateyinstall.ps1
@@ -2,10 +2,11 @@
 $packageName = 'copy-dialog-lunar-lander'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$Shortcut    = 'Copy Dialog Lunar Lander.lnk'
-$ProgramEXE  = 'CopyDialogLunarLander.exe'
-$Checksum	 = 'ACD1B6090D875F7E3EB107E0EBDE337F1DDF12373E57E8680E904D724565DFCA'
+$toolsDir    		= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Shortcut    		= 'Copy Dialog Lunar Lander.lnk'
+$ProgramEXE  		= 'CopyDialogLunarLander.exe'
+$checksum_file	    = 'ACD1B6090D875F7E3EB107E0EBDE337F1DDF12373E57E8680E904D724565DFCA'
+$checksumType_file	= 'sha256'
 
 $packageArgs = @{
     PackageName    = $packageName
@@ -13,7 +14,7 @@ $packageArgs = @{
     Destination    = $toolsDir
 }
 
-Get-ChocolateyWebFile -PackageName 'copy-dialog-lunar-lander' -FileFullPath "$toolsPath\copy-dialog-lunar-lander-v1.0.zip" -Url 'https://github.com/Sanakan8472/copy-dialog-lunar-lander/releases/download/v1.0/copy-dialog-lunar-lander-v1.0.zip'
+Get-ChocolateyWebFile -Checksum $checksum_file -ChecksumType $checksumType_file -PackageName 'copy-dialog-lunar-lander' -FileFullPath "$toolsPath\copy-dialog-lunar-lander-v1.0.zip" -Url 'https://github.com/Sanakan8472/copy-dialog-lunar-lander/releases/download/v1.0/copy-dialog-lunar-lander-v1.0.zip'
 Get-ChocolateyUnzip @packageArgs
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\copy-dialog-lunar-lander\$ProgramEXE" -WorkingDirectory "$toolsDir"
