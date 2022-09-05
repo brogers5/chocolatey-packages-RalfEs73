2f4453cb5ea180589483df531cf933d34ca50c71
RalfEs73 <ralf@essbaumer.de>
2022-09-05 17:36:50 +0200

    First Version

--- copy-dialog-lunar-lander/tools/chocolateybeforemodify.ps1
+++ copy-dialog-lunar-lander/tools/chocolateybeforemodify.ps1
@@ -1,8 +1,8 @@
-﻿$CheckProcess = Get-Process | Where-Object {$_.ProcessName -eq "Chia"}
+﻿$CheckProcess = Get-Process | Where-Object {$_.ProcessName -eq "CopyDialogLunarLander"}
 If($CheckProcess -eq $null){
 	Write-Host "Process is not currently running."
 	} 
 	else {
-	Write-Host "Process is currently running. Killing chia process."
-    Stop-Process -Name Chia
+	Write-Host "Process is currently running. Killing process."
+    Stop-Process -Name CopyDialogLunarLander
 	}
--- copy-dialog-lunar-lander/tools/chocolateyuninstall.ps1
+++ copy-dialog-lunar-lander/tools/chocolateyuninstall.ps1
@@ -2,5 +2,14 @@
 $packageName = 'copy-dialog-lunar-lander' 
 $Shortcut    = 'Copy Dialog Lunar Lander.lnk'
 
+$CheckProcess = Get-Process | Where-Object {$_.ProcessName -eq "CopyDialogLunarLander"}
+If($CheckProcess -eq $null){
+	Write-Host "Process is not currently running."
+	} 
+	else {
+	Write-Host "Process is currently running. Killing process."
+    Stop-Process -Name CopyDialogLunarLander
+	}
+	
 Remove-Item -Path "$env:Public\Desktop\$Shortcut"
 Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut"
