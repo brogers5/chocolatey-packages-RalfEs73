d401dae3640029992c19de8507fbccb3eb60e80d
RalfEs73 <ralf@essbaumer.de>
2022-09-05 16:50:49 +0200

    First Version

---
+++ copy-dialog-lunar-lander/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'copy-dialog-lunar-lander'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Shortcut    = 'Copy Dialog Lunar Lander.lnk'
+$ProgramEXE  = 'CopyDialogLunarLander.exe'
+
+$packageArgs = @{
+    PackageName    = $packageName
+    FileFullPath   = "$toolsPath\copy-dialog-lunar-lander-v1.0.zip"
+    Destination    = $toolsDir
+}
+
+Get-ChocolateyWebFile -PackageName 'copy-dialog-lunar-lander' -FileFullPath "$toolsPath\copy-dialog-lunar-lander-v1.0.zip" -Url 'https://github.com/Sanakan8472/copy-dialog-lunar-lander/releases/download/v1.0/copy-dialog-lunar-lander-v1.0.zip'
+Get-ChocolateyUnzip @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\Mule\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\Mule\$ProgramEXE"    
---
+++ copy-dialog-lunar-lander/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'copy-dialog-lunar-lander' 
+$Shortcut    = 'Copy Dialog Lunar Lander.lnk'
+
+Remove-Item -Path "$env:Public\Desktop\$Shortcut"
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut"
