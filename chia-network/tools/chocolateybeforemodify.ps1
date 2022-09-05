f39015139f409072ce32ce1e3720c6c1f8b604b0
RalfEs73 <ralf@essbaumer.de>
2022-09-05 17:36:55 +0200

    Update chocolateybeforemodify.ps1

--- chia-network/tools/chocolateybeforemodify.ps1
+++ chia-network/tools/chocolateybeforemodify.ps1
@@ -1,8 +1,8 @@
-﻿$CheckProcess = Get-Process | Where-Object {$_.ProcessName -eq "CopyDialogLunarLander"}
+﻿$CheckProcess = Get-Process | Where-Object {$_.ProcessName -eq "Chia"}
 If($CheckProcess -eq $null){
 	Write-Host "Process is not currently running."
 	} 
 	else {
 	Write-Host "Process is currently running. Killing chia process."
-    Stop-Process -Name CopyDialogLunarLander
+    Stop-Process -Name Chia
 	}
