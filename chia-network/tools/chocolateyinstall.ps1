fb2edd54753be7a6111d8d3a2d88918da9b55ea3
RalfEs73 <ralf@essbaumer.de>
2022-09-24 19:31:04 +0200

    New Version 1.6.0

--- chia-network/tools/chocolateyinstall.ps1
+++ chia-network/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Chia-Network/chia-blockchain/releases/download/1.5.1/ChiaSetup-1.5.1.exe'
+$url        = 'https://github.com/Chia-Network/chia-blockchain/releases/download/1.6.0/ChiaSetup-1.6.0.exe'
 $process	= "Chia"
 
 $packageArgs = @{
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'chia-network*'
 
-  checksum      = 'AB7995B32C2B021FB8AF5507C07B64DF974305A16CFF2EF0EBC68EE660BAB1E1'
+  checksum      = '84BDA6C78D3D7D892FFAFD38417A54E5D799823C1546717C15FD3354F47B2421'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
