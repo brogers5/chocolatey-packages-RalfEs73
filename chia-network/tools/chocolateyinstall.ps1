b8aec92bb31ef3a8e8695b31d7a32d7d7382b4c2
RalfEs73 <ralf@essbaumer.de>
2023-03-30 19:45:24 +0200

    New Version 1.7.1

--- chia-network/tools/chocolateyinstall.ps1
+++ chia-network/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Chia-Network/chia-blockchain/releases/download/1.7.0/ChiaSetup-1.7.0.exe'
+$url        = 'https://github.com/Chia-Network/chia-blockchain/releases/download/1.7.1/ChiaSetup-1.7.1.exe'
 # $url        = 'https://download.chia.net/latest/Setup-Win64.exe'
 $process	= "Chia"
 
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'chia-network*'
 
-  checksum      = '38dbf88e72716d9b1ff27469c39ce7b98f8135f558092414981fcde987fa6723'
+  checksum      = '55EA612DC3CE00D382012E59C5FB7EED2E795BD714D481D369D9C0F65B484758'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
