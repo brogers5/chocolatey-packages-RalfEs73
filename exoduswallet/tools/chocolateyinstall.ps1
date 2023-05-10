427e8ee987a9999a6527b3c3f65e225f20fc7586
RalfEs73 <ralf@essbaumer.de>
2023-05-10 09:32:06 +0200

    New Version 23.5.8

--- exoduswallet/tools/chocolateyinstall.ps1
+++ exoduswallet/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://downloads.exodus.com/releases/exodus-windows-x64-23.4.24.exe'
+$url64      = 'https://downloads.exodus.com/releases/exodus-windows-x64-23.5.8.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'exoduswallet*'
-  checksum64    = '56cb6f7d83a11a9fc17a3e13d0a1b0f797fef3eea58d513063a06341cc599cb5'
+  checksum64    = 'a20aee6d2abd3e1b1eba73e4cfb817358a93e2b0eaf7c8454999b27fb0fcab98'
   checksumType64= 'sha256'
   silentArgs   = '/s /v"/qn"'
 }
