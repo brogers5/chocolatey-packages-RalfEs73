f4820a1215fac2de55ef848e56955f66d494b947
RalfEs73 <ralf@essbaumer.de>
2023-05-19 20:26:21 +0200

    New Version 2.58.0

--- ledger-live/tools/chocolateyinstall.ps1
+++ ledger-live/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://download.live.ledger.com/ledger-live-desktop-2.55.0-win-x64.exe'
+$url64      = 'https://download.live.ledger.com/ledger-live-desktop-2.58.0-win-x64.exe'
 
 
 $packageArgs = @{
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'ledger-live*'
-  checksum64    = 'D723AB7A4D88AB15D65700AEF30B70FD4BAF66F07CE34E6B0D3004B465C51F2A'
+  checksum64    = 'BC253B483A2235C85D25C9A4590EB2E39F6E839773FDADE2D31CF617B8CEE594'
   checksumType64= 'sha256'
   silentArgs   = '/S'
 }
