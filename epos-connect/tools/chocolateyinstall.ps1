3a85ff58e8678e80e80dd547d2c70d7f1b49506a
RalfEs73 <ralf@essbaumer.de>
2023-07-08 11:35:50 +0200

    New Version 7.6.1.42114

--- epos-connect/tools/chocolateyinstall.ps1
+++ epos-connect/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.eposaudio.com/globalassets/___image-library/_enterprise/files/english/epos-connect/epos-connect-7.6.0.40680/eposconnect_7.6.0.40680.exe'
+$url        = 'https://www.eposaudio.com/globalassets/___image-library/_enterprise/files/english/epos-connect/epos-connect-7.6.1/eposconnect_7.6.1.42114.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'epos-connect*'
-  checksum      = '5E9F7ABBCD5B316AE7D36B2BCC523524B40DE7E36BF13BE7DF4C5B98927ECF3A'
+  checksum      = '157DF526AD85B926E1DD4C7E504502F025B10BE660D284DD778150C34AAB320E'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
